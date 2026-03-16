ad6b5c330d5c3aafed065980b29e7f7ad76ac15f
Haroon Ahamed <56659007+Terraria200@users.noreply.github.com>
2026-03-15 18:15:49 -0700

    Add user input for dataset and algorithm selection
    Main program for the user to choose the file containing the dataset to search on and choose which of the 2 search algorithms to use.

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ import numpy as np
 def load_data(file):
     data = np.loadtxt(file)
     return data[:,0], data[:,1:]
+
+
+# Main program
+file = input("Enter dataset filename: ")
+labels, features = load_data(file)
+
+print("\nChoose algorithm")
+print("1) Forward Selection")
+print("2) Backward Elimination")
+
+choice = int(input())
+
+if choice == 1:
+    forward(labels, features)
+else:
+    backward(labels, features)
